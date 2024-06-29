9920175ee4b3d447a4913cdcd178090bb56e4f75
kristijan <kristijan@bricksandlogic.com>
2024-06-30 00:24:19 +0100

    init pages

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <h1>Welcome to Vue</h1>
-    <AppAlert>
-      Alert
-    </AppAlert>
-<!--    <NuxtRouteAnnouncer />-->
-<!--    <NuxtWelcome />-->
-  </div>
+  <NuxtPage />
+<!--  <div>-->
+<!--    <h1>Welcome to Vue</h1>-->
+<!--    <AppAlert>-->
+<!--      Alert-->
+<!--    </AppAlert>-->
+<!--&lt;!&ndash;    <NuxtRouteAnnouncer />&ndash;&gt;-->
+<!--&lt;!&ndash;    <NuxtWelcome />&ndash;&gt;-->
+<!--  </div>-->
 </template>
